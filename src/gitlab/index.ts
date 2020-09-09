674ed669685fa371846f23d68b1baa5c2ef957de
Arthur BABEY <git@ababey.com>
2020-09-09 17:34:36 +0200

    fix

--- src/gitlab/index.ts
+++ src/gitlab/index.ts
@@ -8,7 +8,7 @@ export {
 const MR_EVENT = ["merge_request"];
 const MR_OPEN_ACTION = "open";
 
-export const isEventAnMrOpening = (evt: any) => {
+export const isEventAnMrOpening = (evt: any) => 
   !MR_EVENT.includes(evt.event_type) &&
     evt.object_attributes.action !== MR_OPEN_ACTION;
-};
+;
--- src/index.ts
+++ src/index.ts
@@ -10,38 +10,47 @@ import {
 } from "./gitlab";
 
 import { applyRules, getRulesForMr } from "./rules";
+import debug from "./debug";
 
-// 1 - Get and parse the event
-const stdinBuffer = fs.readFileSync(0);
-const gitLabEvent = JSON.parse(stdinBuffer);
+try {
+  // 1 - Get and parse the event
+  const stdinBuffer = fs.readFileSync(0);
+  const gitLabEvent = JSON.parse(stdinBuffer);
 
-// 4 - Get MergeRequest reviewer
-// 5 - Find and select them ramdomly
-// 6 - Assign merge request
+  // 4 - Get MergeRequest reviewer
+  // 5 - Find and select them ramdomly
+  // 6 - Assign merge request
 
-// 2 - If it's not a new mergeRequest - Exit
-if (isEventAnMrOpening) {
-  process.exit();
+  // 2 - If it's not a new mergeRequest - Exit
+  if (isEventAnMrOpening(gitLabEvent)) {
+    process.exit();
+  }
+
+  const project_id = gitLabEvent.project.id;
+  const mrIid = gitLabEvent.object_attributes.iid;
+  const target_branch = gitLabEvent.object_attributes.target_branch;
+
+  const rules = getRulesForMr({ project_id, target_branch });
+
+  // 3 - Check if the repos has a tag enabling auto assignment
+  getGroupMembers({ groupId: config.groupId }, (body) => {
+    if (!body || !body.length) {
+      return;
+    }
+
+    const members = applyRules(rules, body);
+
+    if (members && members.length) {
+      setMergeRequestAssignee(
+        {
+          projectId: project_id,
+          mrIid,
+          assignees: members.map(({ id }) => id),
+        },
+        () => {}
+      );
+    }
+  });
+} catch (error) {
+  debug(error.message || error.toString());
 }
-
-const project_id = gitLabEvent.project.id;
-const mrIid = gitLabEvent.object_attributes.iid;
-const target_branch = gitLabEvent.object_attributes.target_branch;
-
-const rules = getRulesForMr({ project_id, target_branch });
-
-// 3 - Check if the repos has a tag enabling auto assignment
-getGroupMembers({ groupId: config.groupId }, (body) => {
-  if (!body || !body.length) {
-    return;
-  }
-
-  const members = applyRules(rules, body);
-
-  if (members && members.length) {
-    setMergeRequestAssignee(
-      { projectId: project_id, mrIid, assignees: members.map(({ id }) => id) },
-      () => {}
-    );
-  }
-});
